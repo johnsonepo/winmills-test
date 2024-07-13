90ad6d83c3a11635e6893c1c3e158824621f1b3c
Johnson Epo <145839868+johnsonepo@users.noreply.github.com>
2024-07-13 14:33:03 +0400

    Update add-new.php

--- backend/views/site/user/add-new.php
+++ backend/views/site/user/add-new.php
@@ -12,21 +12,20 @@ $this->title = 'Add New User';
 
 <div class="user-create">
 
-    <div class="user-form w-1/3">
+    <div class="user-form sm:w-full md:w-1/2 lg:w-1/3 mx-auto">
 
-    <?php if (Yii::$app->session->hasFlash('success')): ?>
-    <?php endif; ?>
+        <?php if (Yii::$app->session->hasFlash('success')): ?>
+        <?php endif; ?>
 
-    <?php if (Yii::$app->session->hasFlash('error')): ?>
-        
-    <?php endif; ?>
+        <?php if (Yii::$app->session->hasFlash('error')): ?>
+        <?php endif; ?>
 
         <?php $form = ActiveForm::begin(['action' => ['user/create']]); ?>
 
-        <?= $form->field($model, 'username')->textInput(['maxlength' => true, 'value' => ''])?>
-        <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'value' => ''])?>
-        <?= $form->field($model, 'password')->passwordInput(['maxlength' => true, 'value' => '']) ?>
-        
+        <?= $form->field($model, 'username')->textInput(['maxlength' => true, 'value' => '']) ?>
+        <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'value' => '', 'type' => 'email']) ?>
+        <?= $form->field($model, 'password_hash')->passwordInput(['maxlength' => true, 'value' => ''])->label('Password') ?>
+
         <?= $form->field($model, 'role')->dropDownList(
             \yii\helpers\ArrayHelper::map($roles, 'name', 'name'),
             ['prompt' => 'Select Role']
